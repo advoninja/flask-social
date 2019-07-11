d581ee0b403cef45769cded9ae753831ae19871b
shameem <shameem@systemss-MacBook-Pro-2.local>
2019-07-11 19:45:46 +0530

    remove access_token_method in facebook

--- flask_social/providers/facebook.py
+++ flask_social/providers/facebook.py
@@ -21,7 +21,6 @@ config = {
     'base_url': 'https://graph.facebook.com/',
     'request_token_url': None,
     'access_token_url': '/oauth/access_token',
-    'access_token_method':'GET',
     'authorize_url': 'https://www.facebook.com/dialog/oauth',
     'request_token_params': {
         'scope': 'email'
